c1ee01131ef14dd054ebeb0f071920c7a608a9ef
MarieDevulder <marie.devulder@student.junia.com>
2024-03-08 16:44:20 +0100

    repimpage

--- src/components/boutons.js
+++ src/components/boutons.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import MoyenneParMatiere from "./moyennes_par_matiere";
 import VueDEnsembleEleves from "./vue_d_ensemble_eleves";
-import NotesParEleve from "./notes_par_eleve";
 import SelectionEleve from "./selection_eleve";
 
 const Boutons = () => {
@@ -9,29 +8,21 @@ const Boutons = () => {
 
   return (
     <div className="w-100 h-100 flex-row justify-content-between">
-      <h1 className="pt-5">Type de rélevé de note</h1>
+      <h1 className="py-5">Choisissez ce que vous voulez afficher</h1>
       <button
-        className="glass1 text-white"
+        className="glass1 text-white fs-3"
         onClick={() => setAffichage("moyennesMatière")}
       >
         Moyennes par matière
       </button>
       <button
-        className="glass1 text-white"
+        className="glass1 text-white fs-3"
         onClick={() => setAffichage("notesÉlèves")}
       >
-        Notes élèves
+        Vue d'ensemble des élèves
       </button>
-      {/* <button
-        className="glass1 text-white"
-        onClick={() => setAffichage("notesParÉlève")}
-      >
-        Notes par élève
-      </button> */}
-      {/* je veux qu'en cliquant sur le bouton "Notes par élève" on affiche un component de selection d'eleve et que quand cet eleve est selectionné la on affiche le composant "NotesParEleve" */}
-
       <button
-        className="glass1 text-white"
+        className="glass1 text-white fs-3"
         onClick={() => setAffichage("SelectionEleve")}
       >
         Notes par élève
@@ -39,7 +30,6 @@ const Boutons = () => {
 
       {affichage === "moyennesMatière" && <MoyenneParMatiere />}
       {affichage === "notesÉlèves" && <VueDEnsembleEleves />}
-      {affichage === "notesParÉlève" && <NotesParEleve />}
       {affichage === "SelectionEleve" && <SelectionEleve />}
     </div>
   );
--- src/components/selection_eleve.js
+++ src/components/selection_eleve.js
@@ -5,6 +5,7 @@ const SelectionEleve = () => {
   const [eleves, setEleves] = useState([]);
   const [eleveChoisi, setEleveChoisi] = useState("");
   const [notesEleve, setNotesEleve] = useState([]);
+  const [moyenne, setMoyenne] = useState(null);
 
   useEffect(() => {
     const driver = neo4j.driver(
@@ -19,6 +20,7 @@ const SelectionEleve = () => {
         `
         MATCH (e:Élève)
         RETURN e.nom AS nom, e.prénom AS prénom
+        ORDER BY nom, prénom
       `
       )
       .then((result) => {
@@ -29,10 +31,7 @@ const SelectionEleve = () => {
         setEleves(eleveChoisiArray);
       })
       .catch((error) => {
-        console.error(
-          "Erreur lors de la récupération des élèves: ",
-          error
-        );
+        console.error("Erreur lors de la récupération des élèves: ", error);
       })
       .finally(() => {
         session.close();
@@ -41,15 +40,13 @@ const SelectionEleve = () => {
   }, []);
 
   const handleSelectChange = (event) => {
-    var nom = event.target.value.split(' ')[0];
-    var prenom = event.target.value.split(' ')[1];
+    var nom = event.target.value.split(" ")[0];
+    var prenom = event.target.value.split(" ")[1];
     setEleveChoisi(event.target.value);
     handleEleveClick(nom, prenom);
   };
 
   const handleEleveClick = (nom, prenom) => {
-    // console.log("Nom: ", nom);
-    // console.log("Prénom: ", prenom);
     // Charger les notes de l'élève sélectionné
     const driver = neo4j.driver(
       "bolt://localhost:7687",
@@ -70,14 +67,16 @@ const SelectionEleve = () => {
           a.valeur AS note
 
         RETURN
-          *;
+          *
+        ORDER BY matiere
 
-      `.replace("NAME", nom).replace("FIRSTNAME", prenom),
+      `
+          .replace("NAME", nom)
+          .replace("FIRSTNAME", prenom),
 
         { nom, prenom }
       )
       .then((result) => {
-        // console.log(result);
         const eleveChoisiArray = result.records.map((record) => ({
           nom: record.get("nom"),
           prénom: record.get("prénom"),
@@ -85,6 +84,12 @@ const SelectionEleve = () => {
           note: record.get("note"),
         }));
         setNotesEleve(eleveChoisiArray);
+
+        // Calculer la moyenne
+        const moy =
+          eleveChoisiArray.reduce((acc, curr) => acc + curr.note.low, 0) /
+          eleveChoisiArray.length;
+        setMoyenne(moy);
       })
       .catch((error) => {
         console.error(
@@ -92,65 +97,75 @@ const SelectionEleve = () => {
           error
         );
       })
-      
+
       .finally(() => {
         session.close();
         driver.close();
       });
   };
 
+  const getAppreciation = (moyenne) => {
+    if (moyenne >= 0 && moyenne <= 5) {
+      return "Médiocre, il est plus que temps de se bouger";
+    } else if (moyenne > 5 && moyenne < 8) {
+      return "Très insuffisant, il faut remonter la pente";
+    } else if (moyenne >= 8 && moyenne < 10) {
+      return "Insuffisant, il faut faire davantage d'efforts";
+    } else if (moyenne >= 10 && moyenne < 12) {
+      return "Suffisant mais il faut pousser un peu plus";
+    } else if (moyenne >= 12 && moyenne < 14) {
+      return "Correct, poursuivre";
+    } else if (moyenne >= 14 && moyenne < 16) {
+      return "Très satisfaisant, continuer";
+    } else if (moyenne >= 16 && moyenne < 19) {
+      return "Excellent, à poursuivre";
+    } else if (moyenne > 19) {
+      return "Quelle excellence, surclassment ?";
+    } else {
+      return "Appréciation indéterminée";
+    }
+  };
+
   return (
-    <div className="w-auto m-5 px-2 d-flex flex-row justify-content-center">
+    <div className="w-auto m-5 d-flex flex-row justify-content-center">
       {/* Menu déroulant permettant de sélectionner un élève */}
-      <select 
-        onChange={handleSelectChange} 
-        className="glass1 h-25 text-white"
-        >
+      <select
+        onChange={handleSelectChange}
+        className="glass1 h-25 w-auto text-white fs-4 text-center"
+      >
         <option value="">Choisissez un élève</option>
         {eleves.map((eleve, index) => (
-          <option
-            key={index}
-            value={String(eleve.nom + ' ' + eleve.prénom)}
-          >
+          <option key={index} value={String(eleve.nom + " " + eleve.prénom)}>
             {eleve.nom} {eleve.prénom}
           </option>
         ))}
       </select>
-        {/* <button onClick={() => console.log("Nom: ", eleve.nom, "Prénom: ", eleve.prénom)}>Voir les notes</button> */}
 
-      {/* Affichage des notes de l'élève sélectionné */}
-      
-    
-        <h3>Notes de l'élève : {eleveChoisi}</h3>
-        <div className="w-auto glass-table px-2">
+      <div className="w-50 glass-table px-2 d-flex flex-column justify-content-between m-0">
         <table>
           <thead>
             <tr>
-              <th className="pb-7"> Nom </th>
-              <th className="pb-7"> Prénom </th>
               <th className="pb-7"> Matière </th>
               <th className="pb-7"> Note </th>
+
+              <th colSpan="2" className="text-uppercase text-warning pb-7">
+                Moyenne générale : <span className="fs-4">{moyenne}</span>{" "}
+                <br /> {eleveChoisi && getAppreciation(moyenne)}
+              </th>
             </tr>
           </thead>
           <tbody>
             {notesEleve.map((eleveChoisi, index) => (
               <tr key={index}>
-                {/* {
-                  String(
-                    eleveChoisi.id
-                  ) /* obligée de le mettre en string car pb sinon */
-                }
-                <td>{eleveChoisi.nom}</td>
-                <td>{eleveChoisi.prénom}</td>
                 <td>{eleveChoisi.matiere}</td>
                 <td>{eleveChoisi.note.low}</td>
               </tr>
             ))}
             {eleveChoisi.length === 0 && (
-            <tr>
-              <td colSpan="2">Aucune donnée à afficher</td>
-            </tr>
-          )}
+              <tr>
+                <td colSpan="2">Veuillez sélectionner un élève :)</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
